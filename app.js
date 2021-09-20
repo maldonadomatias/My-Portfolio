84815ef1768619159851d191f1a6c531f2bdbac1
maldonadomatias <molporron@gmail.com>
2021-09-20 16:06:12 -0300

    mail

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const nodeMail = require("nodemailer");
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +18,53 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Nodemailer
+
+async function mainMail(name, email, subject, message) {
+  const transporter = await nodeMail.createTransport({
+    service: "gmail",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const mailOption = {
+    from: process.env.GMAIL_USER,
+    to: process.env.EMAIL,
+    subject: subject,
+    html: `You got a message from 
+    Email : ${email}
+    Name: ${name}
+    Message: ${message}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    return Promise.resolve("Message Sent Successfully!");
+  } catch (error) {
+    console.log(error)
+    return Promise.reject(error);
+  }
+}
+
+app.post("/", async (req, res, next) => {
+  const { yourname, youremail, yoursubject, yourmessage } = req.body;
+  try {
+    await mainMail(yourname, youremail, yoursubject, yourmessage);
+    
+    res.send("Message Successfully Sent!");
+  } catch (error) {
+    res.render("error");
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
